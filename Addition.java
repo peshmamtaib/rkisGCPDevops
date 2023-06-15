618949ad315dc5c06402e2fed1f9f6dfc8a18895
peshmamtaib <md.taib21@gmail.com>
2023-06-15 15:14:32 +0530

    Added the first java Program to find sum of two numbers

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{
+	public void static main(String [] args)
+	{
+		int n1,n2,sum;
+		n1=10;
+		n2=20;
+		sum=n1+n2;
+		System.out.println("The Sum of Two Numbers is : +"Sum);
+	}
+}
