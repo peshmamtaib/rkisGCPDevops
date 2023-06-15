c044bfde1179082fa4d26869c246e1a6f8703785
peshmamtaib <md.taib21@gmail.com>
2023-06-15 15:20:11 +0530

    Changed the variables name in the program

--- Addition.java
+++ Addition.java
@@ -2,10 +2,10 @@ class Addition
 {
 	public void static main(String [] args)
 	{
-		int n1,n2,sum;
+		int num1,num2,sum;
 		n1=10;
 		n2=20;
-		sum=n1+n2;
+		sum=num1+num2;
 		System.out.println("The Sum of Two Numbers is : +"Sum);
 	}
 }
