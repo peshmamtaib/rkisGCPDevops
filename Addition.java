3aa13af2a46e3107d921a801c0e5c1bf6025a24f
peshmamtaib <md.taib21@gmail.com>
2023-06-16 11:33:17 +0530

    Revert "Modified the changes in the addition file"
    This reverts commit ab4919dea3f2315716aeb8aa3dc7de865899269e.

--- Addition.java
+++ Addition.java
@@ -2,12 +2,12 @@ class Addition
 {
 	public void static main(String [] args)
 	{
-		int number1,number2,sumOf;
-		number1=10;
-		number2=20;
-		sumOf=number1+number2;
+		int n1,n2,sumOf;
+		n1=10;
+		n2=20;
+		sumOf=num1+num2;
 		System.out.println("The Sum of Two Numbers is : +"sumOf);
-		System.out.println("The given Number value to Number1 is :+"number1);
+		System.out.println("The given Number value to Num1 is :+"n1);
 		System.out.println("Hello all, Thank You");
 	}
 }
